07ed072ced72e74dfc4f7a0c5fe25d8d3861796a
guidojw <guidojwdejong@hotmail.com>
2020-03-02 18:11:48 +0100

    Change collector's event binding order

--- app/services/discord.js
+++ app/services/discord.js
@@ -10,13 +10,13 @@ exports.prompt = async (channel, author, message) => {
     const filter = (reaction, user) => (reaction.emoji.name === '✅' || reaction.emoji.name === '🚫') && user.id
         === author.id
     const collector = message.createReactionCollector(filter, { time: 60000 })
-    collector.on('collect', collector.stop)
     const promise = new Promise(resolve => {
         collector.on('end', collected => {
             const reaction = collected.first()
             resolve(reaction && reaction.emoji.name === '✅')
         })
     })
+    collector.on('collect', collector.stop)
     await message.react('✅')
     await message.react('🚫')
     return promise
