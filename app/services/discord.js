ff0f7e07a0d32a6e5345a5ab9b39c42d5262f706
Guido de Jong <guidojwdejong@hotmail.com>
2020-03-02 18:13:07 +0100

    Enhancement/commando (#11)
    * Change data and configuration format
    
    * Refactor controllers to use Commando
    
    * Set up command permissions
    
    * Delete old commands
    
    * Implement clear and set activity command
    
    * Correct clear command's name
    
    * Change method signatures
    
    * Improve and finalize commands
    
    * Change collector's event binding order

--- app/services/discord.js
+++ app/services/discord.js
@@ -1,45 +1,23 @@
 'use strict'
-const InputError = require('../errors/input-error')
-
-exports.hasRole = (member, name) => {
-    return member.roles.some(role => role.name === name)
-}
-
 exports.isAdmin = (member, adminRoles) => {
-    for (const role of adminRoles) {
-        if (exports.hasRole(member, role)) return true
+    for (const roleId of adminRoles) {
+        if (member.roles.has(roleId)) return true
     }
     return false
 }
 
-exports.addRole = async (member, name) => {
-    const role = member.guild.roles.find(role => role.name === name)
-    if (role) await member.addRole(role)
-}
-
-exports.removeRole = async (member, name) => {
-    const role = member.roles.find(role => role.name === name)
-    if (role) await member.removeRole(role)
-}
-
-exports.prompt = (channel, author, ...options) => {
-    return new Promise(async (resolve, reject) => {
-        const message = await channel.send(...options)
-        await message.react('✅')
-        await message.react('🚫')
-        try {
-            const filter = (reaction, user) => (reaction.emoji.name === '✅' || reaction.emoji.name === '🚫') && user.id
-                === author.id
-            const collected = await message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+exports.prompt = async (channel, author, message) => {
+    const filter = (reaction, user) => (reaction.emoji.name === '✅' || reaction.emoji.name === '🚫') && user.id
+        === author.id
+    const collector = message.createReactionCollector(filter, { time: 60000 })
+    const promise = new Promise(resolve => {
+        collector.on('end', collected => {
             const reaction = collected.first()
-            resolve(reaction.emoji.name === '✅')
-        } catch (err) {
-            reject(new InputError('Prompt timed out.'))
-        }
-        message.delete()
+            resolve(reaction && reaction.emoji.name === '✅')
+        })
     })
-}
-
-exports.getIdFromArgument = argument => {
-    return argument.slice(2, -1)
+    collector.on('collect', collector.stop)
+    await message.react('✅')
+    await message.react('🚫')
+    return promise
 }
