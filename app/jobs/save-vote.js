cea0b7288ed09490e0421b9ce292ce4635c705a4
guidojw <guidojwdejong@hotmail.com>
2020-03-15 20:33:21 +0100

    Hotfix instantiate options as map

--- app/jobs/save-vote.js
+++ app/jobs/save-vote.js
@@ -1,5 +1,5 @@
 'use strict'
-module.exports = async guild => {
+module.exports = guild => {
     const voteData = guild.getData('vote')
     guild.setData('vote', voteData)
     if (voteData.timer.end <= new Date().getTime()) guild.stopJob('saveVoteJob')
