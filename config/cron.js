642393058abb76d5149f5488e250042990a18ec1
guidojw <guidojwdejong@hotmail.com>
2020-05-06 21:55:43 +0200

    Add embed colors

--- config/cron.js
+++ config/cron.js
@@ -4,16 +4,16 @@ const updateTimerJob = require('../app/jobs/update-timer')
 const premiumMembersReportJob = require('../app/jobs/premium-members-report')
 
 module.exports = {
-  'saveVoteJob': {
-    'expression': '*/2 * * * *',
-    'job': saveVoteJob
+  saveVoteJob: {
+    expression: '*/2 * * * *', // https://crontab.guru/#*/2_*_*_*_*
+    job: saveVoteJob
   },
-  'updateTimerJob': {
-    'expression': '*/2 * * * *',
-    'job': updateTimerJob
+  updateTimerJob: {
+    expression: '*/2 * * * *', // https://crontab.guru/#*/2_*_*_*_*
+    job: updateTimerJob
   },
-  'premiumMembersReportJob': {
-    'expression': '0 12 */1 * *',
-    'job': premiumMembersReportJob
+  premiumMembersReportJob: {
+    expression: '0 12 */1 * *', // https://crontab.guru/#0_12_*/1_*_*
+    job: premiumMembersReportJob
   }
 }
