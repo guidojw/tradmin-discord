ef9d7a65c6475da7c904c3d6d6ec5f28fe1721df
guidojw <guidojwdejong@hotmail.com>
2021-03-10 18:01:13 +0100

    Change boostinfo months to 30 days and add notice

--- app/commands/main/boost-info.js
+++ app/commands/main/boost-info.js
@@ -1,6 +1,7 @@
 'use strict'
 const pluralize = require('pluralize')
 const Command = require('../../controllers/command')
+const timeHelper = require('../../helpers/time')
 
 const { MessageEmbed } = require('discord.js')
 
@@ -15,38 +16,29 @@ module.exports = class BoostInfoCommand extends Command {
         key: 'member',
         prompt: 'Whose boost info do you want to know?',
         type: 'member',
-        default: ''
+        default: message => message.member
       }]
     })
   }
 
-  async execute (message, { member }, guild) {
-    if (!member) {
-      member = message.member
-    }
+  async execute (message, { member }) {
     if (!member.premiumSince) {
       return message.reply(`${message.argString ? 'Member is not' : 'You\'re not'} a booster.`)
     }
     const now = new Date()
-    const premiumDate = member.premiumSince
-    let months = now.getMonth() - premiumDate.getMonth() + (now.getFullYear() - premiumDate.getFullYear()) * 12
-    let days = now.getDate() - premiumDate.getDate()
-    if (days < 0) {
-      const daysInMonth = new Date(premiumDate.getFullYear(), premiumDate.getMonth() + 1, 0).getDate()
-      days += daysInMonth
-      months--
-    }
-    const years = Math.floor(months / 12)
-    months %= 12
+    const diffDays = timeHelper.diffDays(member.premiumSince, now)
+    const months = Math.floor(diffDays / 30)
+    const days = diffDays % 30
     const emoji = this.client.bot.mainGuild.guild.emojis.cache.find(emoji => emoji.name.toLowerCase() === 'boost')
+
     if (member.user.partial) {
       await member.user.partial.fetch()
     }
-
     const embed = new MessageEmbed()
-      .setTitle(`${member.user.tag}${emoji ? ` ${emoji}` : ''}`)
+      .setTitle(`${member.user.tag} ${emoji || ''}`)
       .setThumbnail(member.user.displayAvatarURL())
-      .setDescription(`Has been boosting this server for ${years > 0 ? `**${years}** ${pluralize('year', years)}, ` : ''}**${months}** ${pluralize('month', months)} and **${days}** ${pluralize('day', days)}!`)
+      .setDescription(`Has been boosting this server for **${pluralize('month', months, true)}** and **${pluralize('day', days, true)}**!`)
+      .setFooter('* Discord Nitro months are 30 days long.')
       .setColor(0xff73fa)
     return message.replyEmbed(embed)
   }
